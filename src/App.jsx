7445714782cc94cd0bcf34b6d78221ceb082a30b
saiful-islam <md2215566@gmail.com>
2023-09-15 09:14:18 +0600

    credit hour complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,28 @@ import Carts from './Components/Carts/Carts'
 function App() {
 
   const [carts, setCarts] = useState([]);
-  const [credit, setCredit] = useState(0);
+  const [credits, setCredits] = useState(0);
   const [price, setPrice] = useState(0);
 
-  const handleSelect = (card, hour, purchase) =>{
-    setCarts([...carts, card]);
-    setCredit(credit + hour);
-    setPrice(price + purchase);
+  const handleSelect = (card, purchase) => {
+    // console.log(credits.length)
+    const isExist = carts.find(cart => cart.id === card.id);
+    if (isExist) {
+      return alert('cart is exist')
+    } else {
+      let hour = card.credit;
+      carts.forEach(cart => {
+        hour += cart.credit
+      })
+      if (hour > 20) {
+        return alert('your maximum credit hour is 20. you cant add more...')
+      } else {
+        setCarts([...carts, card]);
+        setCredits(hour); 
+        setPrice(price + purchase);
+      }
+
+    }
   }
 
   return (
@@ -21,7 +36,7 @@ function App() {
       <h1 className='text-center text-[#1C1B1B] text-3xl font-bold pt-10'>Course Registration</h1>
       <div className='flex flex-col md:flex-row mt-12'>
         <Cards handleSelect={handleSelect}></Cards>
-        <Carts carts={carts} credit={credit} price={price}></Carts>
+        <Carts carts={carts} credit={credits} price={price}></Carts>
       </div>
     </div>
   )
